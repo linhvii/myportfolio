c462a6466fddd9ab44ee1566983bb50e7ec0beba
linhvii <144689925+linhvii@users.noreply.github.com>
2023-10-04 03:51:27 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Graphics Designer.", "UI-UX Designer.","Apps Designer."],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
